98cfb3b8cf51f274bddb2a088a1c943a9a9b98c1
Fabrício Muniz <contato@fabriciomuniz.com>
2018-07-07 09:21:33 -0300

    initial commit

---
+++ src/main/java/com/fabriciomuniz/checkseriesapi/CheckseriesApiApplication.java
@@ -0,0 +1,12 @@
+package com.fabriciomuniz.checkseriesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckseriesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheckseriesApiApplication.class, args);
+    }
+}
